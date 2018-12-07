021a4d7858f3a4ea91ce14499c9d3628a3fdbaf0
Jamie <flare2399@gmail.com>
2018-12-07 23:14:48 +0000

    Fix samp cog

--- samp/samp.py
+++ samp/samp.py
@@ -10,14 +10,19 @@ class Samp(commands.Cog):
     @commands.command()
     async def samp(self, ctx, ip: str):
         """SA:MP Server Lookup"""
-        ip = ip.split(":")
-        if ip[1] == "":
-            ip[1] = 7777
-        r = requests.post("http://monitor.sacnr.com/api/?IP={}&Port={}&Action=info&Format=json".format(ip[0], ip[1]))
+        port = 0
+        serv_ip = ip
+        if ip[len(ip) - 5] == ":":
+            ips = ip.split(":")
+            port = ips[1]
+            serv_ip = ips[0]
+        else:
+            port = "7777"
+        r = requests.post("http://monitor.sacnr.com/api/?IP={}&Port={}&Action=info&Format=json".format(serv_ip, port))
         colour = discord.Color.from_hsv(random.random(), 1, 1)  # Random Hex Value for embed colour.
         embed = discord.Embed(title="SA:MP Server Information", colour=colour)
         embed.add_field(name="Server:", value=r.json()['Hostname'], inline=True)
-        embed.add_field(name="IP:", value='{}'.format(":".join(ip)), inline=True)
+        embed.add_field(name="IP:", value='{}'.format(ip), inline=True)
         embed.add_field(name="Players:", value=r.json()['Players'], inline=True)
         embed.add_field(name="Version:", value=r.json()['Version'], inline=True)
         embed.add_field(name="Website:", value=r.json()['WebURL'], inline=True)
