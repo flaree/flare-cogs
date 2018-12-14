7794c251be953b1917db75e7db43815f482d92ba
Jamie <flare2399@gmail.com>
2018-12-14 00:46:23 +0000

    [Overwatch] Add setprofile

--- overwatch/overwatch.py
+++ overwatch/overwatch.py
@@ -1,8 +1,11 @@
-from redbot.core import commands
+from redbot.core import commands, Config
 import discord
 import requests
 import random
 from redbot.core.utils.chat_formatting import pagify
+
+defaults = {"Profiles": {},
+            "Region": {}}
 
 
 class Overwatch(commands.Cog):
@@ -13,8 +16,47 @@ class Overwatch(commands.Cog):
         """Overwatch Commands"""
         pass
 
+    def __init__(self):
+        self.database = Config.get_conf(self, identifier=4268355870, force_registration=True)
+        self.database.register_guild(**defaults)
+
+    @ow.command()
+    async def setprofile(self, ctx, account: str, region: str):
+        """Set profile for automatic lookup via ow profile"""
+        async with self.database.guild(ctx.guild).Profiles() as profiles:
+            key1 = ctx.author.id
+            account = account.replace("#", "-")
+            profiles[key1] = account
+        async with self.database.guild(ctx.guild).Region() as regions:
+            key1 = ctx.author.id
+            regions[key1] = region
+        await ctx.send(f"Profile and region updated successfully.")
+
     @ow.command(alias="stats")
-    async def profile(self, ctx, account: str, region: str):
+    async def profile(self, ctx):
+        """OW Profile Stats using set account"""
+        data = await self.database.guild(ctx.guild).all()
+        profile = ctx.author.id
+        try:
+            r = requests.get(
+                f"https://ow-api.com/v1/stats/pc/{data['Region']['{}'.format(profile)]}/{data['Profiles']['{}'.format(profile)]}/profile")
+            colour = discord.Color.from_hsv(random.random(), 1, 1)
+            embed = discord.Embed(title="Overwatch Profile Information", colour=colour)
+            embed.set_author(name=r.json()['name'], icon_url=r.json()['icon'])
+            embed.set_thumbnail(url=r.json()['icon'])
+            embed.add_field(name="Name:", value=r.json()['name'], inline=True)
+            embed.add_field(name="Level:", value=r.json()['level'], inline=True)
+            embed.add_field(name="Prestige:", value=r.json()['prestige'], inline=True)
+            if not r.json()['private']:
+                embed.add_field(name="Games Won:", value=r.json()['gamesWon'], inline=True)
+            else:
+                embed.set_footer(text="Please set your profile status to public for more stats.")
+            await ctx.send(embed=embed)
+        except KeyError:
+            await ctx.send("Ensure you've set the correct name and region via [p]ow setprofile")
+
+    @ow.command()
+    async def lookup(self, ctx, account: str, region: str):
         """OW Profile Stats - Account must include the ID. Ensure profile is public for full stats"""
         account = account.replace("#", "-")
         try:
