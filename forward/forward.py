36c6b7a450bbc746bae28e6c938b82c5d25f0cc9
Jamie <flare2399@gmail.com>
2019-01-16 14:13:12 +0000

    [Forward] Streamline cog.

--- forward/forward.py
+++ forward/forward.py
@@ -8,19 +8,22 @@ class Forward(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_message(self, message):
+    async def sendowner(self, embed2):
         await self.bot.is_owner(discord.Object(id=None))
         owner = self.bot.get_user(self.bot.owner_id)
-        if message.guild != None:
+        await owner.send(embed=embed2)
+
+    async def on_message(self, message):
+        if message.guild is None:
             return
         if message.channel.recipient.id == self.bot.owner_id:
             return
         if message.author == self.bot.user:
             embed = discord.Embed(title=f"Sent PM to {message.channel.recipient}({message.channel.recipient.id}).",
                                   timestamp=message.created_at)
-            await owner.send(embed=embed)
+            await self.sendowner(embed)
         else:
             if message.content[0] not in await self.bot.get_prefix(message):
                 embed = discord.Embed(description=message.content, timestamp=message.created_at)
                 embed.set_author(name=message.author, icon_url=message.author.avatar_url)
-                await owner.send(embed=embed)
+                await self.sendowner(embed)
