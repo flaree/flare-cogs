7515a520f0ca7dbaf6f11b1d7bde4eff90e185cd
flare - Jamie <flare2399@gmail.com>
2019-06-04 01:00:29 +0100

    k

--- b44/b44.py
+++ b44/b44.py
@@ -1,6 +1,7 @@
 from redbot.core import commands, Config, checks
 import discord
 import aiohttp
+import asyncio
 from io import BytesIO
 
 
@@ -14,8 +15,16 @@ class B44(commands.Cog):
             "manorhouse": "https://steamuserimages-a.akamaihd.net/ugc/797613650483487212/2461F1492D5DEEB65036FC83EC95CEF66C93106B/",
             "invasion": "https://steamuserimages-a.akamaihd.net/ugc/788606529604715254/76A59A564B0F352B86634032182CEEFD32767BD0/",
             "savoia": "https://steamuserimages-a.akamaihd.net/ugc/788606529607778839/7724CF76A02B1BDBE051FF3F56678126B99FDA73/",
+            "docks": "https://steamuserimages-a.akamaihd.net/ugc/788606529611310823/14F34983CEBABFDC5FA061B8D26BC528A963B3A1/",
         }
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
+
+    async def updatechannel(self):
+        resp = await self.get(
+            "http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v0001/?appid=489940"
+        )
+        channel = self.bot.get_channel(584017422059700224)
+        await channel.edit(name="Battalion Players: ~{}".format(resp["response"]["player_count"]))
 
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
@@ -49,4 +58,4 @@ class B44(commands.Cog):
         resp = await self.get(
             "http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v0001/?appid=489940"
         )
-        await ctx.send("{} recently played!".format(resp["response"]["player_count"]))
+        await ctx.send("{} people have played/are playing Battalion!".format(resp["response"]["player_count"]))
