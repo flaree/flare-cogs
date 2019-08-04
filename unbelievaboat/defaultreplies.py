39e869a5e2b56daaa0475eebd2bd0d1e0b4420a4
flare <flare2399@gmail.com>
2019-08-04 16:29:07 +0100

    [unbelievaboat] First commit.

---
+++ unbelievaboat/defaultreplies.py
@@ -0,0 +1,33 @@
+work = [
+    "You work in the mines for {amount}.",
+    "You cut down a tree for {amount}.",
+    "You drive women to the store for {amount}.",
+    "You work as a software developer and earn {amount}",
+    "You work as a behaviour consultant and earn {amount}",
+    "You are paid {amount} to clean up hair off the ground at your local hairdressers.",
+    "You work as an Red-DiscordBot developer and earn {amount}.",
+    "You work for a private military company, earning {amount}",
+    "You work as a courier for the day. You are paid {amount}",
+    "You did a 30 minute workout. While jogging you found {amount} on the floor.",
+    "You shine people's shoes in class and they give you {amount}.",
+    "You shine people's shoes in the mall and earn {amount}.",
+    "You worked at the office overtime for {amount}.",
+    "You worked at the office and earned {amount}.",
+    "{amount} falls out of the sky and hits you on the head.",
+    "You help a newcomer set up a camp and he pays you {amount}",
+    "You work at a summer camp and earn {amount}."
+    "You work in a local bookmakers and earn {amount}.",
+    "You work in your local shop and get paid {amount}.",
+    "You earn {amount} from a football bet.",
+]
+crimes = [
+    "You rob a bank for {amount}.",
+    "You steal a car and sell it for {amount}.",
+    "You steal and car and scrap it for {amount}.",
+    "You rob a liqor store for {amount}.",
+    "You rob an old woman for {amount}.",
+    "You steal a kids bike from the playground for {amount}.",
+    "You and your posy rob a bank for {amount}.",
+    "You see a man digging up a grave. He pays you {amount} to help him.",
+    "You sell drugs on the corner block for {amount}.",
+]
