29a99055f4119c884f96f3b584b3e924d2842d44
flare <flare2399@gmail.com>
2020-04-12 14:33:37 +0100

    blackify

--- faceit/funcs.py
+++ faceit/funcs.py
@@ -3,6 +3,7 @@ from redbot.core import commands
 import discord
 import contextlib
 
+
 async def match_info(
     ctx: commands.Context,
     pages: list,
@@ -18,11 +19,11 @@ async def match_info(
             await message.remove_reaction(emoji, ctx.author)
     command = ctx.bot.get_command("faceit match")
     await ctx.send(
-        "Click the X on the match menu to return to the menu before.",
-        delete_after=20,
+        "Click the X on the match menu to return to the menu before.", delete_after=20,
     )
     await ctx.invoke(command, match_id=message.embeds[0].to_dict()["fields"][0]["value"])
     return await menu(ctx, pages, controls, message=message, page=page, timeout=timeout)
+
 
 async def account_stats(
     ctx: commands.Context,
@@ -39,11 +40,11 @@ async def account_stats(
             await message.remove_reaction(emoji, ctx.author)
     command = ctx.bot.get_command("faceit stats")
     await ctx.send(
-        "Click the X on the in-depth statistics to return to the menu before.",
-        delete_after=20,
+        "Click the X on the in-depth statistics to return to the menu before.", delete_after=20,
     )
     await ctx.invoke(command, game=message.embeds[0].to_dict()["fields"][4]["name"].lower())
     return await menu(ctx, pages, controls, message=message, page=page, timeout=timeout)
+
 
 async def account_matches(
     ctx: commands.Context,
