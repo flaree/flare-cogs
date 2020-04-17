cd7a84dd65377eb10cfc68bb4f9e86d5bc4cd27c
flare - Jamie <flare2399@gmail.com>
2020-04-18 00:27:20 +0100

    [covid] 0.1.0 - new stats

--- covid/covid.py
+++ covid/covid.py
@@ -12,7 +12,7 @@ from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
 class Covid(commands.Cog):
     """Covid-19 (Novel Coronavirus Stats)."""
 
-    __version__ = "0.0.8dev1"
+    __version__ = "0.1.0"
 
     def format_help_for_context(self, ctx):
         """Thanks Sinbad."""
@@ -121,10 +121,19 @@ class Covid(commands.Cog):
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 Global Statistics",
+                timestamp=datetime.datetime.utcfromtimestamp(data["updated"] / 1000),
             )
             embed.add_field(name="Cases", value=humanize_number(data["cases"]))
             embed.add_field(name="Deaths", value=humanize_number(data["deaths"]))
             embed.add_field(name="Recovered", value=humanize_number(data["recovered"]))
+            embed.add_field(name="Critical", value=humanize_number(data["critical"]))
+            embed.add_field(name="Active", value=humanize_number(data["active"]))
+            embed.add_field(
+                name="Affected Countries", value=humanize_number(data["affectedCountries"])
+            )
+            embed.add_field(name="Cases Today", value=humanize_number(data["todayCases"]))
+            embed.add_field(name="Deaths Today", value=humanize_number(data["todayDeaths"]))
+            embed.add_field(name="Total Tests", value=humanize_number(data["tests"]))
             await ctx.send(embed=embed)
         else:
             async with ctx.typing():
@@ -137,30 +146,63 @@ class Covid(commands.Cog):
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 | {} Statistics".format(data["country"]),
+                timestamp=datetime.datetime.utcfromtimestamp(data["updated"] / 1000),
             )
+            embed.set_thumbnail(url=data["countryInfo"]["flag"])
             embed.add_field(name="Cases", value=humanize_number(data["cases"]))
             embed.add_field(name="Deaths", value=humanize_number(data["deaths"]))
             embed.add_field(name="Recovered", value=humanize_number(data["recovered"]))
             embed.add_field(name="Cases Today", value=humanize_number(data["todayCases"]))
             embed.add_field(name="Deaths Today", value=humanize_number(data["todayDeaths"]))
-            embed.add_field(name="Critical Condition", value=humanize_number(data["critical"]))
+            embed.add_field(name="Critical", value=humanize_number(data["critical"]))
+            embed.add_field(name="Active", value=humanize_number(data["active"]))
+            embed.add_field(name="Total Tests", value=humanize_number(data["tests"]))
+            embed.add_field(name="\u200b", value="\u200b")
+            await ctx.send(embed=embed)
+
+    @covid.command()
+    async def yesterday(self, ctx, *, country: str):
+        """Show the statistics from yesterday for a country"""
+        async with ctx.typing():
+            data = await self.get(self.api + "v2/countries/{}?yesterday=1".format(country))
+            error = data.get("failed")
+            if error is not None:
+                return await ctx.send(error)
+            if not data:
+                return await ctx.send("No data available.")
+            embed = discord.Embed(
+                color=await self.bot.get_embed_color(ctx.channel),
+                title="Covid-19 | Yesterday | {} Statistics".format(data["country"]),
+                timestamp=datetime.datetime.utcfromtimestamp(data["updated"] / 1000),
+                description="Stats are from **YESTERDAY**.",
+            )
+            embed.set_thumbnail(url=data["countryInfo"]["flag"])
+            embed.add_field(name="Cases", value=humanize_number(data["cases"]))
+            embed.add_field(name="Deaths", value=humanize_number(data["deaths"]))
+            embed.add_field(name="Recovered", value=humanize_number(data["recovered"]))
+            embed.add_field(name="Cases Yesterday", value=humanize_number(data["todayCases"]))
+            embed.add_field(name="Deaths Yesterday", value=humanize_number(data["todayDeaths"]))
+            embed.add_field(name="Critical", value=humanize_number(data["critical"]))
+            embed.add_field(name="Active", value=humanize_number(data["active"]))
+            embed.add_field(name="Total Tests", value=humanize_number(data["tests"]))
+            embed.add_field(name="\u200b", value="\u200b")
             await ctx.send(embed=embed)
 
     @covid.command()
     async def todaycases(self, ctx):
         """Show the highest cases from countrys today"""
         async with ctx.typing():
-            data = await self.get(self.api + "v2/countries")
+            data = await self.get(self.api + "v2/countries?sort=todayCases")
             if isinstance(data, dict):
                 error = data.get("failed")
                 if error is not None:
                     return await ctx.send(error)
             if not data:
                 return await ctx.send("No data available.")
-            data = sorted(data, key=lambda x: x["todayCases"], reverse=True)
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 | Highest Cases Today | {}".format(data[0]["country"]),
+                timestamp=datetime.datetime.utcfromtimestamp(data[0]["updated"] / 1000),
             )
             embed.add_field(name="Cases", value=humanize_number(data[0]["cases"]))
             embed.add_field(name="Deaths", value=humanize_number(data[0]["deaths"]))
@@ -171,44 +213,20 @@ class Covid(commands.Cog):
             await ctx.send(embed=embed)
 
     @covid.command()
-    async def today(self, ctx):
-        """Statistics for today."""
-        async with ctx.typing():
-            data = await self.get(self.api + "v2/countries")
-            if isinstance(data, dict):
-                error = data.get("failed")
-                if error is not None:
-                    return await ctx.send(error)
-            if not data:
-                return await ctx.send("No data available.")
-            todayDeaths = 0
-            todayCases = 0
-            for i in range(len(data)):
-                todayDeaths += int(data[i]["todayDeaths"])
-                todayCases += int(data[i]["todayCases"])
-            embed = discord.Embed(
-                title="Covid-19 Statistics for Today",
-                color=await self.bot.get_embed_color(ctx.channel),
-            )
-            embed.add_field(name="Cases Today", value=humanize_number(todayCases))
-            embed.add_field(name="Deaths Today", value=humanize_number(todayDeaths))
-            await ctx.send(embed=embed)
-
-    @covid.command()
     async def todaydeaths(self, ctx):
         """Show the highest deaths from countrys today"""
         async with ctx.typing():
-            data = await self.get(self.api + "v2/countries")
+            data = await self.get(self.api + "v2/countries?sort=todayDeaths")
             if isinstance(data, dict):
                 error = data.get("failed")
                 if error is not None:
                     return await ctx.send(error)
             if not data:
                 return await ctx.send("No data available.")
-            data = sorted(data, key=lambda x: x["todayDeaths"], reverse=True)
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 | Highest Deaths Today | {}".format(data[0]["country"]),
+                timestamp=datetime.datetime.utcfromtimestamp(data[0]["updated"] / 1000),
             )
             embed.add_field(name="Cases", value=humanize_number(data[0]["cases"]))
             embed.add_field(name="Deaths", value=humanize_number(data[0]["deaths"]))
@@ -222,17 +240,17 @@ class Covid(commands.Cog):
     async def highestcases(self, ctx):
         """Show the highest cases from countrys overall"""
         async with ctx.typing():
-            data = await self.get(self.api + "v2/countries")
+            data = await self.get(self.api + "v2/countries?sort=cases")
             if isinstance(data, dict):
                 error = data.get("failed")
                 if error is not None:
                     return await ctx.send(error)
             if not data:
                 return await ctx.send("No data available.")
-            data = sorted(data, key=lambda x: x["cases"], reverse=True)
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 | Highest Cases Overall | {}".format(data[0]["country"]),
+                timestamp=datetime.datetime.utcfromtimestamp(data[0]["updated"] / 1000),
             )
             embed.add_field(name="Cases", value=humanize_number(data[0]["cases"]))
             embed.add_field(name="Deaths", value=humanize_number(data[0]["deaths"]))
@@ -253,10 +271,10 @@ class Covid(commands.Cog):
                     return await ctx.send(error)
             if not data:
                 return await ctx.send("No data available.")
-            data = sorted(data, key=lambda x: x["deaths"], reverse=True)
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 | Highest Deaths Overall | {}".format(data[0]["country"]),
+                timestamp=datetime.datetime.utcfromtimestamp(data[0]["updated"] / 1000),
             )
             embed.add_field(name="Cases", value=humanize_number(data[0]["cases"]))
             embed.add_field(name="Deaths", value=humanize_number(data[0]["deaths"]))
@@ -272,17 +290,17 @@ class Covid(commands.Cog):
         if amount > 20 or amount < 0:
             return await ctx.send("Invalid amount. Please choose between an amount between 1-20.")
         async with ctx.typing():
-            data = await self.get(self.api + "v2/countries")
+            data = await self.get(self.api + "v2/countries?sort=cases")
             if isinstance(data, dict):
                 error = data.get("failed")
                 if error is not None:
                     return await ctx.send(error)
             if not data:
                 return await ctx.send("No data available.")
-            data = sorted(data, key=lambda x: x["cases"], reverse=True)
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 | Top {} Cases ".format(amount),
+                timestamp=datetime.datetime.utcfromtimestamp(data[0]["updated"] / 1000),
             )
             for i in range(amount):
                 msg = f'**Cases**: {humanize_number(data[i]["cases"])}\n**Deaths**: {humanize_number(data[i]["deaths"])}\n**Recovered**: {humanize_number(data[i]["recovered"])}\n**Cases Today**: {humanize_number(data[i]["todayCases"])}\n**Deaths**: {humanize_number(data[i]["todayDeaths"])}\n**Critical**: {humanize_number(data[i]["critical"])}'
@@ -295,17 +313,17 @@ class Covid(commands.Cog):
         if amount > 20 or amount < 0:
             return await ctx.send("Invalid amount. Please choose between an amount between 1-20.")
         async with ctx.typing():
-            data = await self.get(self.api + "v2/countries")
+            data = await self.get(self.api + "v2/countries?sort=todayCases")
             if isinstance(data, dict):
                 error = data.get("failed")
                 if error is not None:
                     return await ctx.send(error)
             if not data:
                 return await ctx.send("No data available.")
-            data = sorted(data, key=lambda x: x["todayCases"], reverse=True)
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 | Top {} Cases Today ".format(amount),
+                timestamp=datetime.datetime.utcfromtimestamp(data[0]["updated"] / 1000),
             )
             for i in range(amount):
                 msg = f'**Cases**: {humanize_number(data[i]["cases"])}\n**Deaths**: {humanize_number(data[i]["deaths"])}\n**Recovered**: {humanize_number(data[i]["recovered"])}\n**Cases Today**: {humanize_number(data[i]["todayCases"])}\n**Deaths**: {humanize_number(data[i]["todayDeaths"])}\n**Critical**: {humanize_number(data[i]["critical"])}'
@@ -318,17 +336,17 @@ class Covid(commands.Cog):
         if amount > 20 or amount < 0:
             return await ctx.send("Invalid amount. Please choose between an amount between 1-20.")
         async with ctx.typing():
-            data = await self.get(self.api + "v2/countries")
+            data = await self.get(self.api + "v2/countries?sort=deaths")
             if isinstance(data, dict):
                 error = data.get("failed")
                 if error is not None:
                     return await ctx.send(error)
             if not data:
                 return await ctx.send("No data available.")
-            data = sorted(data, key=lambda x: x["deaths"], reverse=True)
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 | Top {} Deaths ".format(amount),
+                timestamp=datetime.datetime.utcfromtimestamp(data[0]["updated"] / 1000),
             )
             for i in range(amount):
                 msg = f'**Cases**: {humanize_number(data[i]["cases"])}\n**Deaths**: {humanize_number(data[i]["deaths"])}\n**Recovered**: {humanize_number(data[i]["recovered"])}\n**Cases Today**: {humanize_number(data[i]["todayCases"])}\n**Deaths**: {humanize_number(data[i]["todayDeaths"])}\n**Critical**: {humanize_number(data[i]["critical"])}'
@@ -341,17 +359,17 @@ class Covid(commands.Cog):
         if amount > 20 or amount < 0:
             return await ctx.send("Invalid amount. Please choose between an amount between 1-20.")
         async with ctx.typing():
-            data = await self.get(self.api + "v2/countries")
+            data = await self.get(self.api + "v2/countries?sort=todayDeaths")
             if isinstance(data, dict):
                 error = data.get("failed")
                 if error is not None:
                     return await ctx.send(error)
             if not data:
                 return await ctx.send("No data available.")
-            data = sorted(data, key=lambda x: x["todayDeaths"], reverse=True)
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
                 title="Covid-19 | Top {} Deaths Today ".format(amount),
+                timestamp=datetime.datetime.utcfromtimestamp(data[0]["updated"] / 1000),
             )
             for i in range(amount):
                 msg = f'**Cases**: {humanize_number(data[i]["cases"])}\n**Deaths**: {humanize_number(data[i]["deaths"])}\n**Recovered**: {humanize_number(data[i]["recovered"])}\n**Cases Today**: {humanize_number(data[i]["todayCases"])}\n**Deaths**: {humanize_number(data[i]["todayDeaths"])}\n**Critical**: {humanize_number(data[i]["critical"])}'
@@ -362,30 +380,21 @@ class Covid(commands.Cog):
     async def state(self, ctx, *, state: str):
         """Show stats for a specific state."""
         async with ctx.typing():
-            data = await self.get(self.api + "v2/states")
+            data = await self.get(self.api + "v2/states/{}".format(state))
             if isinstance(data, dict):
                 error = data.get("failed")
                 if error is not None:
                     return await ctx.send(error)
             if not data:
                 return await ctx.send("No data available.")
-            statedata = None
-            for i in range(len(data)):
-                if data[i]["state"].lower() == state.lower():
-                    statedata = i
-            if statedata is None:
-                return await ctx.send("No statistics/State not found.")
             embed = discord.Embed(
                 color=await self.bot.get_embed_color(ctx.channel),
-                title="Covid-19 | USA | {} Statistics".format(data[statedata]["state"]),
+                title="Covid-19 | USA | {} Statistics".format(data["state"]),
             )
-            embed.add_field(name="Cases", value=humanize_number(data[statedata]["cases"]))
-            embed.add_field(name="Deaths", value=humanize_number(data[statedata]["deaths"]))
-            embed.add_field(
-                name="Cases Today", value=humanize_number(data[statedata]["todayCases"])
-            )
-            embed.add_field(
-                name="Deaths Today", value=humanize_number(data[statedata]["todayDeaths"])
-            )
-            embed.add_field(name="Active Cases", value=humanize_number(data[statedata]["active"]))
+            embed.add_field(name="Cases", value=humanize_number(data["cases"]))
+            embed.add_field(name="Deaths", value=humanize_number(data["deaths"]))
+            embed.add_field(name="Cases Today", value=humanize_number(data["todayCases"]))
+            embed.add_field(name="Deaths Today", value=humanize_number(data["todayDeaths"]))
+            embed.add_field(name="Active Cases", value=humanize_number(data["active"]))
+            embed.add_field(name="Total Tests", value=humanize_number(data["tests"]))
             await ctx.send(embed=embed)
