4a359a7029dea1b08aabe23fa4f53e3eae9ae256
flare <flare2399@gmail.com>
2020-05-27 01:48:50 +0100

    move to reddit api instead

--- redditpost/redditpost.py
+++ redditpost/redditpost.py
@@ -1,18 +1,14 @@
 import asyncio
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Optional
 
 import aiohttp
 import discord
-import feedparser
 import validators
-from bs4 import BeautifulSoup
-from redbot.core import commands
-from redbot.core.config import Config
+from redbot.core import Config, commands
 from redbot.core.utils.chat_formatting import pagify
-
-from .htmlparse import html_to_text
+from redbot.core.commands.converter import TimedeltaConverter
 
 log = logging.getLogger("red.flare.redditpost")
 
@@ -23,7 +19,7 @@ class RedditPost(commands.Cog):
     Thanks to mikeshardmind(Sinbad) for the RSS cog as reference
     """
 
-    __version__ = "0.0.1"
+    __version__ = "0.0.2"
 
     def format_help_for_context(self, ctx):
         """Thanks Sinbad."""
@@ -32,19 +28,19 @@ class RedditPost(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=95932766180343808, force_registration=True)
+        self.config = Config.get_conf(self, identifier=959327661803438081, force_registration=True)
         self.config.register_channel(reddits={})
-        self.config.register_global(interval=300)
+        self.config.register_global(delay=300)
         self.session = aiohttp.ClientSession()
         self.bg_loop_task: Optional[asyncio.Task] = None
 
     def init(self):
-        self.bg_loop_task = asyncio.create_task(self.bg_loop())
+        self.bg_loop_task = self.bot.loop.create_task(self.bg_loop())
 
     def cog_unload(self):
         if self.bg_loop_task:
             self.bg_loop_task.cancel()
-        asyncio.create_task(self.session.close())
+        self.bot.loop.create_task(self.session.close())
 
     async def bg_loop(self):
         await self.bot.wait_until_ready()
@@ -70,7 +66,7 @@ class RedditPost(commands.Cog):
                 else:
                     response = await self.fetch_feed(url)
                     feeds[url] = response
-                time = await self.format_send(response, channel, feed["last_post"], sub)
+                time = await self.format_send(response, channel, feed["last_post"])
                 if time is not None:
                     data = {"url": url, "last_post": time}
                     async with self.config.channel(channel).reddits() as feeds:
@@ -82,28 +78,48 @@ class RedditPost(commands.Cog):
         """Reddit auto-feed posting."""
 
     @redditfeed.command()
+    @commands.is_owner()
+    async def delay(
+        self,
+        ctx,
+        seconds: TimedeltaConverter(
+            minimum=timedelta(), maximum=timedelta(seconds=900), default_unit="seconds"
+        ),
+    ):
+        """Set the delay used to check for new content."""
+        time = seconds.total_seconds()
+        await self.config.delay.set(time)
+        await ctx.tick()
+        await ctx.send("This delay will come into effect on the next loop.")
+
+    @redditfeed.command()
     async def add(self, ctx, subreddit: str, channel: Optional[discord.TextChannel] = None):
         """Add a subreddit to post new content from.
 
         Feed must not include the /r/
         """
         channel = channel or ctx.channel
+        async with self.session.get(
+            f"https://www.reddit.com/r/{subreddit}/about.json?sort=new"
+        ) as resp:
+            data = await resp.json()
+            if data["data"]["over18"] and not channel.is_nsfw():
+                return await ctx.send(
+                    "You're trying to add an NSFW subreddit to a SFW channel. Please edit the channel or try another."
+                )
 
         async with self.config.channel(channel).reddits() as feeds:
             if subreddit in feeds:
                 return await ctx.send("That subreddit is already set to post.")
 
-            url = f"https://www.reddit.com/r/{subreddit}/new/.rss"
+            url = f"https://www.reddit.com/r/{subreddit}/new.json?sort=new"
 
             response = await self.fetch_feed(url)
 
             if response is None:
                 return await ctx.send(f"That didn't seem to be a valid rss feed.")
 
-            feeds[subreddit] = {
-                "url": url,
-                "last_post": datetime.fromisoformat("1970-01-01T00:00:00+00:00").timestamp(),
-            }
+            feeds[subreddit] = {"url": url, "last_post": datetime.utcnow().timestamp()}
             await ctx.tick()
 
     @redditfeed.command()
@@ -139,11 +155,14 @@ class RedditPost(commands.Cog):
 
         await ctx.tick()
 
-    async def fetch_feed(self, url: str) -> Optional[feedparser.FeedParserDict]:
+    async def fetch_feed(self, url: str):
         timeout = aiohttp.client.ClientTimeout(total=15)
         try:
             async with self.session.get(url, timeout=timeout) as response:
-                data = await response.read()
+                if response.status == 200:
+                    data = await response.json()
+                else:
+                    return None
         except (aiohttp.ClientError, asyncio.TimeoutError):
             return None
         except Exception as exc:
@@ -152,37 +171,50 @@ class RedditPost(commands.Cog):
                 exc_info=exc,
             )
             return None
+        if data["data"]["dist"] > 0:
+            return data["data"]["children"]
+        return None
 
-        ret = feedparser.parse(data)
-        if ret.bozo:
-            log.debug(f"Feed url: {url} is invalid.")
-            return None
-        return ret
+    async def format_send(self, data, channel, last_post):
+        timestamps = []
+        embeds = []
+        for feed in data:
+            feed = feed["data"]
+            timestamp = feed["created_utc"]
+            if feed["over_18"] and not channel.is_nsfw():
+                timestamps.append(timestamp)
+                continue
+            if timestamp <= last_post:
+                break
+            timestamps.append(timestamp)
 
-    async def format_send(self, feed, channel, lastimg, sub):
-        if not feed.entries:
-            return
-        timestamp = datetime.fromisoformat(feed.entries[0].updated).timestamp()
-        if timestamp <= lastimg:
-            return None
-        soup = BeautifulSoup(feed.entries[0].content[0].value, features="html5lib")
-        desc = html_to_text(feed.entries[0].description)
-        images = soup.find("span")
-        image = images.find("a")["href"]
-        title = f"[{feed.entries[0].title}]({feed.entries[0].link})\n\n" + " ".join(
-            desc.split()[:-5]
-        )
-        if len(title) > 2000:
-            title = title[:2000] + "..."
-        embed = discord.Embed(
-            title=f"New post on r/{sub}", description=title, color=await ctx.embed_color()
-        )
-        embed.set_footer(text=" ".join(desc.split()[-5:-2]))
-        if image[-3:] in ["png", "jpg"]:
-            embed.set_image(url=image)
-        else:
-            if image != feed.entries[0].link:
-                if validators.url(image):
-                    embed.add_field(name="Attachment", value=image)
-        await channel.send(embed=embed)
-        return timestamp
+            desc = feed["selftext"]
+
+            image = feed["url"]
+
+            link = "https://reddit.com" + feed["permalink"]
+
+            title = f"[{feed['title']}]({link})\n\n" + " ".join(desc.split()[:-5])
+
+            if len(title) > 2000:
+                title = title[:2000] + "..."
+            embed = discord.Embed(
+                title=f"New post on r/{feed['subreddit']}",
+                description=title,
+                color=channel.guild.me.color,
+            )
+            embed.set_footer(text=f"Submitted by /u/{feed['author']}")
+            if image[-3:] in ["png", "jpg"]:
+                embed.set_image(url=image)
+            else:
+                if feed["permalink"] not in image:
+                    if validators.url(image):
+                        embed.add_field(name="Attachment", value=image)
+            embeds.append(embed)
+        if timestamps:
+            if embeds:
+                for emb in embeds[::-1]:
+                    await channel.send(embed=emb)
+
+            return timestamps[0]
+        return None
