a44a8c808937e6b182f785b2e63c0dea8f0c4f19
flare <flare2399@gmail.com>
2020-07-23 15:25:30 +0100

    check if leveler is loaded

--- simleague/__init__.py
+++ simleague/__init__.py
@@ -1,6 +1,15 @@
 from .simleague import SimLeague
 
+try:
+    from redbot.core.errors import CogLoadError
+except ImportError:
+    CogLoadError = RuntimeError
+
 
 async def setup(bot):
+    if "Leveler" not in bot.cogs:
+        raise CogLoadError(
+            "A mongodb instance and leveler by fixator/aikaterna is **REQUIRED** for this cog to function."
+        )
     cog = SimLeague(bot)
     bot.add_cog(cog)
