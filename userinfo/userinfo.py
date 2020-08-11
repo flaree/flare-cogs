a2491243a15222bac1ee8ad8f2f87c7348901c3d
flare <flare2399@gmail.com>
2020-08-11 03:06:24 +0100

    userinfo dpy 1.4

--- userinfo/userinfo.py
+++ userinfo/userinfo.py
@@ -1,8 +1,9 @@
 import logging
 
 import discord
-from redbot.core import commands
+from redbot.core import commands, bank
 from redbot.core.utils import AsyncIter
+from redbot.core.utils.chat_formatting import humanize_number
 from redbot.core.utils.common_filters import (
     escape_spoilers_and_mass_mentions,
     filter_invites,
@@ -17,7 +18,7 @@ log = logging.getLogger("red.flare.userinfo")
 class Userinfo(commands.Cog):
     """Replace original Red userinfo command with more details."""
 
-    __version__ = "0.0.4"
+    __version__ = "0.1.0"
 
     def format_help_for_context(self, ctx):
         """Thanks Sinbad."""
@@ -26,6 +27,14 @@ class Userinfo(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.emojis = self.bot.loop.create_task(self.init())
+
+    def cog_unload(self):
+        if self.emojis:
+            self.emojis.cancel()
+
+    async def init(self):
+        await self.bot.wait_until_ready()
         self.status_emojis = {
             "mobile": discord.utils.get(self.bot.emojis, id=725387684113023057),
             "online": discord.utils.get(self.bot.emojis, id=724950463417548890),
@@ -33,6 +42,20 @@ class Userinfo(commands.Cog):
             "dnd": discord.utils.get(self.bot.emojis, id=724950462499127338),
             "offline": discord.utils.get(self.bot.emojis, id=724950462746460271),
             "streaming": discord.utils.get(self.bot.emojis, id=724950551900717066),
+        }
+        self.badge_emojis = {
+            "staff": discord.utils.get(self.bot.emojis, id=706198524156706917),
+            "early_supporter": discord.utils.get(self.bot.emojis, id=706198530837970998),
+            "hypesquad_balance": discord.utils.get(self.bot.emojis, id=706198531538550886),
+            "hypesquad_bravery": discord.utils.get(self.bot.emojis, id=706198532998299779),
+            "hypesquad_brilliance": discord.utils.get(self.bot.emojis, id=706198535846101092),
+            "hypesquad": discord.utils.get(self.bot.emojis, id=706198537049866261),
+            "verified_bot_developer": discord.utils.get(self.bot.emojis, id=706198727953612901),
+            "bug_hunter": discord.utils.get(self.bot.emojis, id=706199712402898985),
+            "bug_hunter_level_2": discord.utils.get(self.bot.emojis, id=706199774616879125),
+            "partner": discord.utils.get(self.bot.emojis, id=706206032216457258),
+            "verified_bot": discord.utils.get(self.bot.emojis, id=706196603748483174),
+            "verified_bot2": discord.utils.get(self.bot.emojis, id=706196604197273640),
         }
 
     async def red_get_data_for_user(self, *, user_id: int):
@@ -59,162 +82,177 @@ class Userinfo(commands.Cog):
     async def userinfo(self, ctx, *, user: discord.Member = None):
         """Show userinfo with some more detail."""
         mod = self.bot.get_cog("Mod")
-        if mod is None:
-            return await ctx.send("This requires the mod cog to be loaded.")
-        author = ctx.author
-        guild = ctx.guild
+        async with ctx.typing():
+            author = ctx.author
+            guild = ctx.guild
 
-        if not user:
-            user = author
-        sharedguilds = {
-            guild async for guild in AsyncIter(self.bot.guilds) if user in guild.members
-        }
-        roles = user.roles[-1:0:-1]
-        names, nicks = await mod.get_names_and_nicks(user)
+            if not user:
+                user = author
+            sharedguilds = {
+                guild async for guild in AsyncIter(self.bot.guilds) if user in guild.members
+            }
+            roles = user.roles[-1:0:-1]
+            names, nicks = await mod.get_names_and_nicks(user)
 
-        joined_at = user.joined_at
-        since_created = (ctx.message.created_at - user.created_at).days
-        if joined_at is not None:
-            since_joined = (ctx.message.created_at - joined_at).days
-            user_joined = joined_at.strftime("%d %b %Y %H:%M")
-        else:
-            since_joined = "?"
-            user_joined = "Unknown"
-        user_created = user.created_at.strftime("%d %b %Y %H:%M")
-        voice_state = user.voice
-        member_number = (
-            sorted(guild.members, key=lambda m: m.joined_at or ctx.message.created_at).index(user)
-            + 1
-        )
-
-        created_on = "{}\n({} days ago)".format(user_created, since_created)
-        joined_on = "{}\n({} days ago)".format(user_joined, since_joined)
-        if user.is_on_mobile():
-            statusemoji = (
-                self.status_emojis["mobile"]
-                if self.status_emojis["mobile"]
-                else "\N{MOBILE PHONE}"
-            )
-        elif any(a.type is discord.ActivityType.streaming for a in user.activities):
-            statusemoji = (
-                self.status_emojis["streaming"]
-                if self.status_emojis["streaming"]
-                else "\N{LARGE PURPLE CIRCLE}"
-            )
-        elif user.status.name == "online":
-            statusemoji = (
-                self.status_emojis["online"]
-                if self.status_emojis["online"]
-                else "\N{LARGE GREEN CIRCLE}"
-            )
-        elif user.status.name == "offline":
-            statusemoji = (
-                self.status_emojis["offline"]
-                if self.status_emojis["offline"]
-                else "\N{MEDIUM WHITE CIRCLE}\N{VARIATION SELECTOR-16}"
-            )
-        elif user.status.name == "dnd":
-            statusemoji = (
-                self.status_emojis["dnd"] if self.status_emojis["dnd"] else "\N{LARGE RED CIRCLE}"
-            )
-        elif user.status.name == "idle":
-            statusemoji = (
-                self.status_emojis["away"]
-                if self.status_emojis["away"]
-                else "\N{LARGE ORANGE CIRCLE}"
-            )
-        activity = "Chilling in {} status".format(user.status)
-        status_string = mod.get_status_string(user)
-
-        if roles:
-
-            role_str = ", ".join([x.mention for x in roles])
-            # 400 BAD REQUEST (error code: 50035): Invalid Form Body
-            # In embed.fields.2.value: Must be 1024 or fewer in length.
-            if len(role_str) > 1024:
-                # Alternative string building time.
-                # This is not the most optimal, but if you're hitting this, you are losing more time
-                # to every single check running on users than the occasional user info invoke
-                # We don't start by building this way, since the number of times we hit this should be
-                # infintesimally small compared to when we don't across all uses of Red.
-                continuation_string = (
-                    "and {numeric_number} more roles not displayed due to embed limits."
+            joined_at = user.joined_at
+            since_created = (ctx.message.created_at - user.created_at).days
+            if joined_at is not None:
+                since_joined = (ctx.message.created_at - joined_at).days
+                user_joined = joined_at.strftime("%d %b %Y %H:%M")
+            else:
+                since_joined = "?"
+                user_joined = "Unknown"
+            user_created = user.created_at.strftime("%d %b %Y %H:%M")
+            voice_state = user.voice
+            member_number = (
+                sorted(guild.members, key=lambda m: m.joined_at or ctx.message.created_at).index(
+                    user
                 )
-
-                available_length = 1024 - len(continuation_string)  # do not attempt to tweak, i18n
-
-                role_chunks = []
-                remaining_roles = 0
-
-                for r in roles:
-                    chunk = f"{r.mention}, "
-                    chunk_size = len(chunk)
-
-                    if chunk_size < available_length:
-                        available_length -= chunk_size
-                        role_chunks.append(chunk)
-                    else:
-                        remaining_roles += 1
-
-                role_chunks.append(continuation_string.format(numeric_number=remaining_roles))
-
-                role_str = "".join(role_chunks)
-
-        else:
-            role_str = None
-
-        data = discord.Embed(
-            description=(status_string or activity) + f"\n\n{len(sharedguilds)} shared servers.",
-            colour=user.colour,
-        )
-
-        data.add_field(name="Joined Discord on", value=created_on)
-        data.add_field(name="Joined this server on", value=joined_on)
-        if role_str is not None:
-            data.add_field(name="Roles", value=role_str, inline=False)
-        if names:
-            # May need sanitizing later, but mentions do not ping in embeds currently
-            val = filter_invites(", ".join(names))
-            data.add_field(name="Previous Names", value=val, inline=False)
-        if nicks:
-            # May need sanitizing later, but mentions do not ping in embeds currently
-            val = filter_invites(", ".join(nicks))
-            data.add_field(name="Previous Nicknames", value=val, inline=False)
-        if voice_state and voice_state.channel:
-            data.add_field(
-                name="Current voice channel",
-                value="{0.mention} ID: {0.id}".format(voice_state.channel),
-                inline=False,
+                + 1
             )
-        data.set_footer(text="Member #{} | User ID: {}".format(member_number, user.id))
 
-        name = str(user)
-        name = " ~ ".join((name, user.nick)) if user.nick else name
-        name = filter_invites(name)
+            created_on = "{}\n({} days ago)".format(user_created, since_created)
+            joined_on = "{}\n({} days ago)".format(user_joined, since_joined)
+            if user.is_on_mobile():
+                statusemoji = (
+                    self.status_emojis["mobile"]
+                    if self.status_emojis["mobile"]
+                    else "\N{MOBILE PHONE}"
+                )
+            elif any(a.type is discord.ActivityType.streaming for a in user.activities):
+                statusemoji = (
+                    self.status_emojis["streaming"]
+                    if self.status_emojis["streaming"]
+                    else "\N{LARGE PURPLE CIRCLE}"
+                )
+            elif user.status.name == "online":
+                statusemoji = (
+                    self.status_emojis["online"]
+                    if self.status_emojis["online"]
+                    else "\N{LARGE GREEN CIRCLE}"
+                )
+            elif user.status.name == "offline":
+                statusemoji = (
+                    self.status_emojis["offline"]
+                    if self.status_emojis["offline"]
+                    else "\N{MEDIUM WHITE CIRCLE}"
+                )
+            elif user.status.name == "dnd":
+                statusemoji = (
+                    self.status_emojis["dnd"]
+                    if self.status_emojis["dnd"]
+                    else "\N{LARGE RED CIRCLE}"
+                )
+            elif user.status.name == "idle":
+                statusemoji = (
+                    self.status_emojis["away"]
+                    if self.status_emojis["away"]
+                    else "\N{LARGE ORANGE CIRCLE}"
+                )
+            activity = "Chilling in {} status".format(user.status)
+            status_string = mod.get_status_string(user)
 
-        avatar = user.avatar_url_as(static_format="png")
-        data.title = f"{statusemoji} {name}"
-        data.set_thumbnail(url=avatar)
+            if roles:
 
-        flags = await discord_py(user)
-        badges = ""
-        for badge in sorted(flags):
-            if badge == "verified_bot":
-                emoji1 = discord.utils.get(self.bot.emojis, id=EMOJIS["verified_bot"])
-                emoji2 = discord.utils.get(self.bot.emojis, id=EMOJIS["verified_bot2"])
-                if emoji1:
-                    emoji = f"{emoji1}{emoji2}"
+                role_str = ", ".join([x.mention for x in roles])
+                # 400 BAD REQUEST (error code: 50035): Invalid Form Body
+                # In embed.fields.2.value: Must be 1024 or fewer in length.
+                if len(role_str) > 1024:
+                    # Alternative string building time.
+                    # This is not the most optimal, but if you're hitting this, you are losing more time
+                    # to every single check running on users than the occasional user info invoke
+                    # We don't start by building this way, since the number of times we hit this should be
+                    # infintesimally small compared to when we don't across all uses of Red.
+                    continuation_string = (
+                        "and {numeric_number} more roles not displayed due to embed limits."
+                    )
+
+                    available_length = 1024 - len(
+                        continuation_string
+                    )  # do not attempt to tweak, i18n
+
+                    role_chunks = []
+                    remaining_roles = 0
+
+                    for r in roles:
+                        chunk = f"{r.mention}, "
+                        chunk_size = len(chunk)
+
+                        if chunk_size < available_length:
+                            available_length -= chunk_size
+                            role_chunks.append(chunk)
+                        else:
+                            remaining_roles += 1
+
+                    role_chunks.append(continuation_string.format(numeric_number=remaining_roles))
+
+                    role_str = "".join(role_chunks)
+
+            else:
+                role_str = None
+
+            data = discord.Embed(
+                description=(status_string or activity)
+                + f"\n\n{len(sharedguilds)} shared servers.",
+                colour=user.colour,
+            )
+
+            data.add_field(name="Joined Discord on", value=created_on)
+            data.add_field(name="Joined this server on", value=joined_on)
+            if role_str is not None:
+                data.add_field(name="Roles", value=role_str, inline=False)
+            if names:
+                # May need sanitizing later, but mentions do not ping in embeds currently
+                val = filter_invites(", ".join(names))
+                data.add_field(name="Previous Names", value=val, inline=False)
+            if nicks:
+                # May need sanitizing later, but mentions do not ping in embeds currently
+                val = filter_invites(", ".join(nicks))
+                data.add_field(name="Previous Nicknames", value=val, inline=False)
+            if voice_state and voice_state.channel:
+                data.add_field(
+                    name="Current voice channel",
+                    value="{0.mention} ID: {0.id}".format(voice_state.channel),
+                    inline=False,
+                )
+            data.set_footer(text="Member #{} | User ID: {}".format(member_number, user.id))
+
+            name = str(user)
+            name = " ~ ".join((name, user.nick)) if user.nick else name
+            name = filter_invites(name)
+
+            avatar = user.avatar_url_as(static_format="png")
+            data.title = f"{statusemoji} {name}"
+            data.set_thumbnail(url=avatar)
+
+            flags = [f.name for f in user.public_flags.all()]
+            badges = ""
+            for badge in sorted(flags):
+                if badge == "verified_bot":
+                    emoji1 = self.badge_emojis["verified_bot"]
+                    emoji2 = self.badge_emojis["verified_bot2"]
+                    if emoji1:
+                        emoji = f"{emoji1}{emoji2}"
+                    else:
+                        emoji = None
                 else:
-                    emoji = None
-            else:
-                emoji = discord.utils.get(self.bot.emojis, id=EMOJIS[badge])
-            if emoji:
-                badges += f"{emoji} {badge.replace('_', ' ').title()}\n"
-            else:
-                badges += f"\N{BLACK QUESTION MARK ORNAMENT}\N{VARIATION SELECTOR-16} {badge.replace('_', ' ').title()}\n"
-        if badges:
-            data.add_field(name="Badges", value=badges)
-        await ctx.send(embed=data)
+                    emoji = self.badge_emojis[badge]
+                if emoji:
+                    badges += f"{emoji} {badge.replace('_', ' ').title()}\n"
+                else:
+                    badges += f"\N{BLACK QUESTION MARK ORNAMENT}\N{VARIATION SELECTOR-16} {badge.replace('_', ' ').title()}\n"
+            if badges:
+                data.add_field(name="Badges", value=badges)
+            if "Economy" in self.bot.cogs:
+                bankstat = f"**Bank**: {str(humanize_number(await bank.get_balance(user)))} {await bank.get_currency_name(ctx.guild)}\n"
+                if "Unbelievaboat" in self.bot.cogs:
+                    cog = self.bot.get_cog("Unbelievaboat")
+                    state = await cog.walletdisabledcheck(ctx)
+                    if not state:
+                        balance = await cog.walletbalance(user)
+                        bankstat += f"**Wallet**: {str(humanize_number(balance))} {await bank.get_currency_name(ctx.guild)}"
+                data.add_field(name="Balance", value=bankstat)
+            await ctx.send(embed=data)
 
 
 try:
@@ -223,7 +261,9 @@ except ImportError:
     CogLoadError = RuntimeError
 
 
-def setup(bot):
+async def setup(bot):
+    if discord.version_info <= (1, 4):
+        raise CogLoadError("This cog requires d.py 1.4+ to work.")
     uinfo = Userinfo(bot)
     if "Mod" not in bot.cogs:
         raise CogLoadError("This cog requires the Mod cog to be loaded.")
