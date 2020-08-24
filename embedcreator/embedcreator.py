59795e8f1901eb1390cab76d87e37e26bf8071d5
flare <flare2399@gmail.com>
2020-08-24 17:37:18 +0100

    data stuffs

--- embedcreator/embedcreator.py
+++ embedcreator/embedcreator.py
@@ -7,6 +7,13 @@ from redbot.core import commands
 
 
 class EmbedCreator(commands.Cog):
+    async def red_get_data_for_user(self, *, user_id: int):
+        # this cog does not story any data
+        return {}
+
+    async def red_delete_data_for_user(self, *, requester, user_id: int) -> None:
+        # this cog does not story any data
+        pass
 
     __version__ = "0.0.1"
 
