ff90ff1235b493f55408fce393c41d43a09aeb19
Jamie <flare2399@gmail.com>
2020-09-06 22:29:42 +0100

    Rewrite botlistpost.

--- botlistspost/botlistspost.py
+++ botlistspost/botlistspost.py
@@ -1,16 +1,17 @@
 import asyncio
 import json
 import logging
+import contextlib
+from typing import Dict
 
 import aiohttp
-from redbot.core import commands
-from redbot.core.utils.chat_formatting import humanize_list
+import discord
+from redbot.core import Config, commands
+from redbot.core.utils.chat_formatting import humanize_list, pagify
+from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
 
-DBLEL = "https://api.discordextremelist.xyz/v2/bot/{BOTID}/stats"
-BFD = "https://botsfordiscord.com/api/bot/{BOTID}"
-DB = "https://discord.bots.gg/api/v1/bots/{BOTID}/stats"
+BOTBLOCK = "https://botblock.org/api"
 BSDC = "https://api.server-discord.com/v2/bots/{BOTID}/stats"
-DBL = "https://discordbotlist.com/api/v1/bots/{BOTID}/stats"
 
 log = logging.getLogger("red.flare.botlistpost")
 
@@ -18,7 +19,7 @@ log = logging.getLogger("red.flare.botlistpost")
 class BotListsPost(commands.Cog):
     """Post data to bot lists. For DBL use Predas cog"""
 
-    __version__ = "0.0.5"
+    __version__ = "0.0.6"
 
     def format_help_for_context(self, ctx):
         """Thanks Sinbad."""
@@ -27,12 +28,10 @@ class BotListsPost(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=95932766180343808, force_registration=True)
+        self.config.register_global(lists={}, version=1)
         self._session = aiohttp.ClientSession(loop=self.bot.loop)
-        self.dbleltoken = None
-        self.bfdtoken = None
-        self.dbtoken = None
         self.bsdctoken = None
-        self.dbltoken = None
         self.post_stats_task = self.bot.loop.create_task(self.post_stats())
 
     async def red_get_data_for_user(self, *, user_id: int):
@@ -44,29 +43,18 @@ class BotListsPost(commands.Cog):
         pass
 
     async def init(self):
-        bfd = await self.bot.get_shared_api_tokens("botsfordiscord")
-        self.bfdtoken = bfd.get("authorization")
-        dblel = await self.bot.get_shared_api_tokens("discordextremelist")
-        self.dbleltoken = dblel.get("authorization")
-        db = await self.bot.get_shared_api_tokens("discordbots")
-        self.dbtoken = db.get("authorization")
         sd = await self.bot.get_shared_api_tokens("serverdiscord")
         self.bsdctoken = sd.get("authorization")
-        dbl = await self.bot.get_shared_api_tokens("discordbotlist")
-        self.dbltoken = dbl.get("authorization")
+        if await self.config.version() < 2:
+            await self.bot.send_to_owners(
+                "Hi, the current cog has been redesigned to use bot block. If you dont wish to use this to manage more lists and save me time then feel free to uninstall. Current setup sites will no longer work (except server-discord) and will need to be readded."
+            )
+            await self.config.version.set(2)
 
     @commands.Cog.listener()
     async def on_red_api_tokens_update(self, service_name, api_tokens):
-        if service_name == "botsfordiscord":
-            self.bfdtoken = {"Authorization": api_tokens.get("authorization")}
-        elif service_name == "discordextremelist":
-            self.dbleltoken = {"Authorization": api_tokens.get("authorization")}
-        elif service_name == "discordbots":
-            self.dbtoken = {"Authorization": api_tokens.get("authorization")}
-        elif service_name == "serverdiscord":
+        if service_name == "serverdiscord":
             self.bsdctoken = {"Authorization": api_tokens.get("authorization")}
-        elif service_name == "discordbotlist":
-            self.dbltoken = {"Authorization": api_tokens.get("authorization")}
 
     def cog_unload(self):
         self.bot.loop.create_task(self._session.close())
@@ -76,50 +64,33 @@ class BotListsPost(commands.Cog):
     async def post_stats(self):
         await self.bot.wait_until_ready()
         await self.init()
+        botid = int(self.bot.user.id)
         while True:
             success = []
             failed = []
             serverc = len(self.bot.guilds)
             shardc = self.bot.shard_count
-            botid = self.bot.user.id
-            if self.dbleltoken is not None:
-                async with self._session.post(
-                    DBLEL.format(BOTID=botid),
-                    headers={"Authorization": self.dbleltoken, "Content-Type": "application/json"},
-                    data=json.dumps({"guildCount": serverc, "shardCount": shardc}),
-                ) as resp:
-                    if resp.status == 200:
-                        success.append("Discord Extreme List")
-                    else:
-                        failed.append(f"Discord Extreme List ({resp.status}")
+            conf = await self.config.lists()
+            if conf:
+                conf["server_count"] = serverc
+                conf["shard_count"] = shardc
+                conf["bot_id"] = botid
 
-            if self.dbtoken is not None:
-                try:
-                    async with self._session.post(
-                        DB.format(BOTID=botid),
-                        headers={
-                            "Authorization": self.dbtoken,
-                            "Content-Type": "application/json",
-                        },
-                        data=json.dumps({"guildCount": serverc, "shardCount": shardc}),
-                    ) as resp:
-                        if resp.status == 200:
-                            success.append("Discord Bots")
-                        else:
-                            failed.append(f"Discord Bots ({resp.status}")
-                except Exception as e:
-                    print(e)
-
-            if self.bfdtoken is not None:
                 async with self._session.post(
-                    BFD.format(BOTID=botid),
-                    headers={"Authorization": self.bfdtoken, "Content-Type": "application/json"},
-                    data=json.dumps({"server_count": serverc}),
-                ) as resp:
-                    if resp.status == 200:
-                        success.append("Bots for Discord")
+                    BOTBLOCK + "/count",
+                    headers={"Content-Type": "application/json"},
+                    data=json.dumps(conf),
+                ) as r:
+                    if r.status == 200:
+                        data = await r.json()
+                        if data.get("success"):
+                            success = list(data["success"].keys())
+                        if data.get("failure"):
+                            for _list in data["failure"]:
+                                failed.append(f"{_list} ({data['failure'][_list][0]})")
                     else:
-                        failed.append(f"Bots for Discord ({resp.status}")
+
+                        failed.append(f"BotBlock ({r.status})")
 
             if self.bsdctoken is not None:
                 async with self._session.post(
@@ -132,38 +103,110 @@ class BotListsPost(commands.Cog):
                         success.append("Server-Discord bot list")
                     else:
                         failed.append(f"Server-Discord bot list ({resp.get('error')})")
-
-            if self.dbltoken is not None:
-                async with self._session.post(
-                    DBL.format(BOTID=botid),
-                    headers={"Authorization": self.dbltoken, "Content-Type": "application/json"},
-                    data=json.dumps({"guilds": serverc, "users": len(self.bot.users)}),
-                ) as resp:
-                    if resp.status == 200:
-                        success.append("Discord Bot List")
-                    else:
-                        failed.append(f"Discord Bot List ({resp.status})")
             if failed:
                 log.info(f"Unable to post data to {humanize_list(failed)}.")
             if success:
                 log.info(f"Successfully posted servercount to {humanize_list(success)}.")
             await asyncio.sleep(1800)
 
+    async def get_lists(self) -> Dict:
+        async with self._session.get(BOTBLOCK + "/lists") as r:
+            if r.status == 200:
+                return await r.json()
+            return {"error": r.status}
+
     @commands.is_owner()
     @commands.command()
     async def botlistpost(self, ctx):
         """Setup for botlistposting"""
         msg = (
-            "This cog currently supports [Bots for Discord](https://botsfordiscord.com), "
-            "[Discord Extreme List](https://discordextremelist.xyz/en-US/), "
-            "[Discord Bots](https://discord.bots.gg), "
-            "[Server-Discord](https://docs.server-discord.com/) and "
-            "[Discord Bot Lists](https://discordbotlist.com).\n"
-            "To set this cog up, please use the following commands:\n"
-            f"`{ctx.clean_prefix}set api botsfordiscord authorization <botsfordiscord apikey>`\n"
-            f"`{ctx.clean_prefix}set api discordextremelist authorization <discordextremelist apikey>`\n"
-            f"`{ctx.clean_prefix}set api discordbots authorization <discordbots apikey>`\n"
+            "This cog currently supports every bot list on [BotBlock](https://botblock.org) along with"
+            " [Server-Discord](https://docs.server-discord.com/)."
+            "To set this cog up for Server-Discord, please use the following command:\n"
             f"`{ctx.clean_prefix}set api serverdiscord authorization <SDC token>`\n"
-            f"`{ctx.clean_prefix}set api discordbotlist authorization <DBL token>`\n"
+            f"Otherwise, you need to type `{ctx.clean_prefix}botlist add <bot list> <token>`\n"
+            f"You can find a list of allowed lists via `{ctx.clean_prefix}botlist list`"
         )
         await ctx.maybe_send_embed(msg)
+
+    @commands.is_owner()
+    @commands.group()
+    async def botlist(self, ctx):
+        """Bot list posting setup"""
+
+    @botlist.command(name="list")
+    async def _list(self, ctx):
+        """Return all available botlists."""
+        lists = await self.get_lists()
+        if lists.get("error"):
+            return await ctx.send(
+                f"An error occured retrieving the lists. Error Code: {lists['error']}"
+            )
+        msg = ""
+        for _list in lists:
+            msg += f"[{_list}]({lists[_list]['url']})\n"
+        embeds = []
+        for page in pagify(msg, page_length=1024):
+            embed = discord.Embed(
+                title="Bot Lists", description=page, colour=await ctx.embed_colour()
+            )
+            embeds.append(embed)
+        await menu(ctx, embeds, DEFAULT_CONTROLS)
+
+    @botlist.command(name="add")
+    async def _add(self, ctx, site: str, *, token: str):
+        """Add a botlist to post stats to."""
+        with contextlib.suppress(discord.NotFound):
+            await ctx.message.delete()
+        lists = await self.get_lists()
+        if lists.get("error"):
+            return await ctx.send(
+                f"An error occured retrieving the lists. Error Code: {lists['error']}"
+            )
+        if site not in lists:
+            return await ctx.send(
+                f"Your list doesn't appear to exist. Ensure its named after how it appears in `{ctx.cleanprefix}botlist list`"
+            )
+        async with self.config.lists() as config:
+            if site in config:
+                config[site] = token
+                await ctx.send(f"{site} token has been updated.")
+            else:
+                config[site] = token
+                await ctx.send(f"{site} has been added to the list.")
+
+    @botlist.command(name="delete")
+    async def _delete(self, ctx, site: str):
+        """Remove a botlist from your setup lists.."""
+        lists = await self.get_lists()
+        if lists.get("error"):
+            return await ctx.send(
+                f"An error occured retrieving the lists. Error Code: {lists['error']}"
+            )
+        if site not in lists:
+            return await ctx.send(
+                f"Your list doesn't appear to exist. Ensure its named after how it appears in `{ctx.cleanprefix}botlist list`"
+            )
+        async with self.config.lists() as config:
+            if site in config:
+                del config[site]
+                await ctx.send(f"{site} has been removed")
+            else:
+                await ctx.send(f"{site} doesnt exist in your setup bot lists.")
+
+    @botlist.command()
+    async def available(self, ctx):
+        """List current setup botlists which are having stats posted to them."""
+        conf = await self.config.lists()
+        if not conf:
+            return await ctx.send("You don't have any lists setup.")
+        msg = ""
+        for _list in conf:
+            msg += f"{_list}\n"
+        embeds = []
+        for page in pagify(msg, page_length=1024):
+            embed = discord.Embed(
+                title="Bot Lists Setup", description=page, colour=await ctx.embed_colour()
+            )
+            embeds.append(embed)
+        await menu(ctx, embeds, DEFAULT_CONTROLS)
