3d2c22933a523ca9582e0f734632e58ef85bfa6e
Jamie <flare2399@gmail.com>
2020-10-01 14:21:53 +0100

    fix command conflict automatic changing

--- dankmemer/__init__.py
+++ dankmemer/__init__.py
@@ -1,15 +1,25 @@
+from asyncio import create_task
+
 from .dankmemer import DankMemer
 
 __red_end_user_data_statement__ = "This cog does not persistently store data about users."
 
-# Thanks Fixator for the changing name code.
-async def setup(bot):
+
+async def setup_after_ready(bot):
+    await bot.wait_until_red_ready()
     cog = DankMemer(bot)
     for name, command in cog.all_commands.items():
-        if bot.get_command(name):
-            command.name = f"dm{command.name}"
-        for alias in command.aliases:
-            if bot.get_command(alias):
-                command.aliases[command.aliases.index(alias)] = f"dm{alias}"
+        if not command.parent:
+            if bot.get_command(name):
+                command.name = f"dm{command.name}"
+            for alias in command.aliases:
+                if bot.get_command(alias):
+                    command.aliases[command.aliases.index(alias)] = f"dm{alias}"
     bot.add_cog(cog)
-    await cog.initalize()
+
+
+# Thanks Fixator for the changing name code.
+
+
+def setup(bot):
+    create_task(setup_after_ready(bot))
