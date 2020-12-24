fdc56900c82a08a4b4ff2d832892d58d20e9b8a4
phenom4n4n <phenom4n4n@gmail.com>
2020-12-23 22:20:55 -0800

    formatting

--- tips/tips.py
+++ tips/tips.py
@@ -28,9 +28,13 @@ async def send(self, content=None, **kwargs):
                 if len(content) + len(tip_msg) > 2000:
                     return await real_send(self, content, **kwargs)
                 else:
-                    content = cog.tip_format.format(content=content, tip_msg=tip_msg, prefix=self.clean_prefix)
+                    content = cog.tip_format.format(
+                        content=content, tip_msg=tip_msg, prefix=self.clean_prefix
+                    )
             else:
-                content = cog.tip_format.replace("{content}", "").format(tip_msg=tip_msg, prefix=self.clean_prefix)
+                content = cog.tip_format.replace("{content}", "").format(
+                    tip_msg=tip_msg, prefix=self.clean_prefix
+                )
     return await real_send(self, content, **kwargs)
 
 
@@ -70,7 +74,7 @@ class Tips(commands.Cog):
     async def tips(self, ctx: commands.Context, toggle: bool) -> None:
         """
         Toggle and setup tips.
-        
+
         Run `[prefix]tips off` to disable tips.
         """
         await self.config.user(ctx.author).toggle.set(toggle)
@@ -96,7 +100,7 @@ class Tips(commands.Cog):
     async def add_tip(self, ctx, *, tip: str):
         """
         Add a tip message.
-        
+
         Append {prefix} to have it formatted with prefix on send.
         """
         async with self.config.tips() as replies:
@@ -164,7 +168,9 @@ class Tips(commands.Cog):
         """
         if formatting:
             await self.config.tip_format.set(formatting)
-            await ctx.channel.send(f"The tip format has been set to:\n{formatting}") # intentionally uses ctx.channel to avoid tips being triggered
+            await ctx.channel.send(
+                f"The tip format has been set to:\n{formatting}"
+            )  # intentionally uses ctx.channel to avoid tips being triggered
         else:
             await self.config.tip_format.clear()
             await ctx.channel.send("The tip format has been reset to the default.")
@@ -172,5 +178,6 @@ class Tips(commands.Cog):
         content = "This is example content of a message with a tip."
         tips = self.message_cache if self.message_cache else ["No tips configured."]
         tip_msg = random.choice(tips).format(prefix=ctx.clean_prefix)
-        await ctx.channel.send(self.tip_format.format(content=content, tip_msg=tip_msg, prefix=ctx.clean_prefix))
-        
+        await ctx.channel.send(
+            self.tip_format.format(content=content, tip_msg=tip_msg, prefix=ctx.clean_prefix)
+        )
