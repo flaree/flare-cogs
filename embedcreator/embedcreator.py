e3ab4da5216f80bf688e51fad487baa463949366
Jamie <flare2399@gmail.com>
2020-12-29 18:30:19 +0000

    [embedcreator] add multi

--- embedcreator/embedcreator.py
+++ embedcreator/embedcreator.py
@@ -40,7 +40,7 @@ class EmbedCreator(commands.Cog):
             for match in all_matches:
                 embeds[match]["author"] = 00000000
 
-    __version__ = "0.1.0"
+    __version__ = "0.1.1"
 
     def format_help_for_context(self, ctx):
         pre_processed = super().format_help_for_context(ctx)
@@ -157,12 +157,12 @@ class EmbedCreator(commands.Cog):
 
     async def validate_data(self, ctx, *, data) -> Optional[discord.Embed]:
         if not isinstance(data, dict):
-                try:
-                    data = json.loads(data)
-                except json.decoder.JSONDecodeError:
-                    return await ctx.send(
-                        "Unable to read JSON, ensure it is correctly formatted and validated."
-                    )
+            try:
+                data = json.loads(data)
+            except json.decoder.JSONDecodeError:
+                return await ctx.send(
+                    "Unable to read JSON, ensure it is correctly formatted and validated."
+                )
         if not isinstance(data, dict):
             await ctx.send("The JSON provided is not in a dictionary format.")
             return False
@@ -255,13 +255,12 @@ class EmbedCreator(commands.Cog):
             return await ctx.send("This embed doesn't exist in this guild.")
         data = embeds_stored[name]["data"]
         await self.build_embed(ctx, data=data, channel=channel)
-    
 
     @embed.command()
     @commands.bot_has_permissions(embed_links=True)
     async def edit(self, ctx, message: discord.Message, *, name: str):
-        """Edit a bot sent message with a new embed. 
-        
+        """Edit a bot sent message with a new embed.
+
         Message format is in messageID format.
         Messages in other channels must follow ChannelID-MessageID format."""
         if message.guild != ctx.guild:
@@ -290,8 +289,8 @@ class EmbedCreator(commands.Cog):
     @embed.command(name="editjson", aliases=["edit-json", "editraw"])
     @commands.bot_has_permissions(embed_links=True)
     async def edit_json(self, ctx, message: discord.Message, *, raw_json: str):
-        """Edit a bot sent message with a new embed from JSON. 
-        
+        """Edit a bot sent message with a new embed from JSON.
+
         Message format is in messageID format.
         Messages in other channels must follow ChannelID-MessageID format."""
         if message.guild != ctx.guild:
@@ -313,8 +312,6 @@ class EmbedCreator(commands.Cog):
                 colour=discord.Color.red(),
             )
             await ctx.send(embed=em)
-        
-    
 
     @embed.command()
     @commands.admin_or_permissions(manage_guild=True)
@@ -372,26 +369,38 @@ class EmbedCreator(commands.Cog):
                 f"The following embed{'s' if len(failed) > 1 else ''} {'does' if len(failed) == 1 else 'do'} not exist in this guild: {humanize_list(failed)}"
             )
         await self.menu_embed(ctx, embeds=embeds)
-        
+
+    @embed.command(name="multi")
+    async def embed_multi(self, ctx, *, embed_names: str):
+        """Send multiple embeds.
+        Must be split using spaces.
+
+        Example Usage: [p]embed multi embed1 embed2 embed3
+        Note: embeds must be saved."""
+        embeds_stored = await self.config.guild(ctx.guild).embeds()
+        failed = []
+        embeds = []
+        for embedname in embed_names.split():
+            if embedname not in embeds_stored:
+                failed.append(embedname)
+            else:
+                embeds.append(embeds_stored[embedname]["data"])
+        if failed:
+            return await ctx.send(
+                f"The following embed{'s' if len(failed) > 1 else ''} {'does' if len(failed) == 1 else 'do'} not exist in this guild: {humanize_list(failed)}"
+            )
+        for embed in embeds:
+            embed = await self.validate_data(ctx, data=embed)
+            if not embed:
+                return
+            await ctx.send(embed=embed)
 
     async def menu_embed(self, ctx, embeds):
         complete_embeds = []
         for data in embeds:
-            if not isinstance(data, dict):
-                try:
-                    data = json.loads(data)
-                except json.decoder.JSONDecodeError:
-                    return await ctx.send(
-                        "Unable to read JSON, ensure it is correctly formatted and validated."
-                    )
-                if not isinstance(data, dict):
-                    return await ctx.send("The JSON provided is not in a dictionary format.")
-            if data.get("embed"):
-                data = data["embed"]
-            if data.get("embeds"):
-                data = data["embeds"][0]
-            if data.get("timestamp"):
-                data["timestamp"] = data["timestamp"].strip("Z")
+            embed = await self.validate_data(ctx, data=data)
+            if not embed:
+                return
             try:
                 embed = discord.Embed().from_dict(data)
             except Exception:
