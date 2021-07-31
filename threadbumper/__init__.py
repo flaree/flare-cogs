6d52c124a0b025c56aa454b6822b012c7e3ea664
flare (Jamie) <flare2399@gmail.com>
2021-07-31 18:20:48 +0100

    change init message

--- threadbumper/__init__.py
+++ threadbumper/__init__.py
@@ -6,9 +6,6 @@ from .threadbumper import ThreadBumper
 
 def setup(bot):
     if discord.__version__[0] == "1":
-        raise CogLoadError(
-            "You need to run the bot with the redbot-py-experimental-repo "
-            "version of discord.py. There is no support for this cog as of yet."
-        )
+        raise CogLoadError("You need to use discord.py 2.0.0 minimum to use this cog")
 
     bot.add_cog(ThreadBumper(bot))
