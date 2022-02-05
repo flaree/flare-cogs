d640d55f0afacc1a3527cbca729c28fda1a5e85a
flaree <flare2399@gmail.com>
2022-02-05 15:53:39 +0000

    Update base jsk + add flags

--- jsk/jishaku_cog.py
+++ jsk/jishaku_cog.py
@@ -1,14 +1,18 @@
+import jishaku
 from jishaku.cog import OPTIONAL_FEATURES, STANDARD_FEATURES
+
+jishaku.Flags.RETAIN = True
+jishaku.Flags.NO_DM_TRACEBACK = True
+jishaku.Flags.FORCE_PAGINATOR = True
 
 
 class Jishaku(*STANDARD_FEATURES, *OPTIONAL_FEATURES):
     """Jishaku ported to Red"""
 
-    __version__ = "0.0.1"
+    __version__ = "0.0.2, Jishaku {}".format(jishaku.__version__)
     __author__ = "flare#0001, Gorialis"
 
     def format_help_for_context(self, ctx):
-        """Thanks Sinbad."""
         pre_processed = super().format_help_for_context(ctx)
         return f"{pre_processed}\nCog Version: {self.__version__}\nAuthor: {self.__author__}"
 
