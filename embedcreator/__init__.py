81cf71049c26021b2022fdf22d480d5e5bf59ca9
flaree <flare2399@gmail.com>
2023-03-04 23:07:12 +0000

    remove old cogs

--- embedcreator/__init__.py
+++
@@ -1,7 +0,0 @@
-from .embedcreator import EmbedCreator
-
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
-
-
-async def setup(bot):
-    await bot.add_cog(EmbedCreator(bot))
--- serverlock/__init__.py
+++
@@ -1,7 +0,0 @@
-from .serverlock import ServerLock
-
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
-
-
-async def setup(bot):
-    await bot.add_cog(ServerLock(bot))
